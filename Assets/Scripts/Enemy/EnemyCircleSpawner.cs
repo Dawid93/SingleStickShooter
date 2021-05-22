7c5bffecacfad1757e76cd67d2ec2b6b0ad14e03
Dave <ddawid@windowslive.com>
2021-05-22 16:09:44 +0200

    Spawn enemies around the player

---
+++ Assets/Scripts/Enemy/EnemyCircleSpawner.cs
@@ -0,0 +1,41 @@
+using System;
+using ObjectPool;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class EnemyCircleSpawner : MonoBehaviour
+{
+	[SerializeField] private DifficultySettings difficultySettings;
+	[SerializeField] private float circleRadius = 25f;
+	[SerializeField] private Transform centerOfCircle;
+
+	private float elapsedTime = 0;
+
+	private void Update()
+	{
+		elapsedTime += Time.deltaTime;
+		if (elapsedTime >= 2)
+		{
+			elapsedTime -= 2;
+			Spawn();
+		}
+	}
+
+	private void Spawn()
+	{
+		var spawnPos = RandomSpawnPos();
+		ObjectPooler.Instance.GetFromPool(Consts.EnemyTag, spawnPos, Quaternion.identity, null);
+	}
+
+	private Vector3 RandomSpawnPos()
+	{
+		var radians = Random.value * 360 * Mathf.Deg2Rad;
+
+		var vertical = Mathf.Sin(radians);
+		var horizontal = Mathf.Cos(radians);
+		
+		var spawnDir = new Vector3(horizontal, 0, vertical);
+		var spawnPos = centerOfCircle.position + spawnDir * circleRadius;
+		return spawnPos;
+	}
+}
