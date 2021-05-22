fad760d20bf98a402594b694e45cdac0ab016582
Dave <ddawid@windowslive.com>
2021-05-23 00:02:12 +0200

    Add tags for spells objects

--- Assets/Scripts/Consts.cs
+++ Assets/Scripts/Consts.cs
@@ -2,4 +2,6 @@ public static class Consts
 {
 	public static string BulletTag = "bullet";
 	public static string EnemyTag = "enemy";
+	public static string OffensiveSpellTag = "offensiveSpell";
+	public static string SlowdownSpellTag = "slowdownSpell";
 }
